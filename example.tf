e6e9a5a79fc89ac72d1eb6afc43d6102d5907c14
John Stockbauer <john@shippable.com>
2017-08-01 21:35:45 -0700

    try provisioners in tf scripts

--- example.tf
+++ example.tf
@@ -5,4 +5,14 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "${var.AMI_ID}"
   instance_type = "t2.micro"
+  key_name = "common-key"
+
+  provisioner "local-exec" {
+    command = "export PUBLIC_IP=${aws_instance.example.public_ip}"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "docker run -d softsrv/minecraft:1.12",
+    ]
+  }
 }
